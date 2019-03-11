c97880f5f0cc64681b4d0eb7734a88d301b12018
klavis0 <klavis@bk.ru>
2019-03-11 17:56:46 +0300

    preparing for next task

---
+++ nextsquare.py
@@ -0,0 +1,16 @@
+import math
+
+
+sq = int(input('Input number '))
+
+
+def find_next_square(sq):
+    if math.sqrt(sq) == int:
+        print('ok')
+    else:
+        print('-1')
+
+
+print(find_next_square(sq))
+
+
